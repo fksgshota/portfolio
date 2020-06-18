dbb536f7600b8c78adbd3ad1399c8f839e6625d1
fksgshota <fksg.shotaness@gmail.com>
2020-06-18 23:00:25 +0900

    footer作成

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,35 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app-bar class="header"></v-app-bar>
+    <div class="first_view">
+      <h1 class="first_view__title">Shota Fukushige's portfolio</h1>
+    </div>
+    <!-- <v-container>
+      <div class>
+        <h2>福重 章太</h2>
+        <p>Fukushige Shota</p>
+        <img src="" alt="">
+        <p>年齢</p>
+        <p>職種</p>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <p>職歴</p>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    </v-container>
+    <SkillCardList></SkillCardList>
+    <p>製作物</p>
+    <p>お問い合わせ</p>-->
+    <v-footer class="footer" absolute="true">
+      <small class="copyright">&copy; 2020 Shota Fukushige</small>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
@@ -58,3 +37,28 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss" scoped>
+.first_view {
+  background-image: url("../public/shota_fukushige.jpg");
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 800px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__title {
+    color: #18ffff;
+    font-size: 90px;
+    text-shadow: 1px 2px 3px grey;
+  }
+}
+
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
