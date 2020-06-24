ca1d1cfde4e42044ec6530faac43bfd680c7318d
fksgshota <fksg.shotaness@gmail.com>
2020-06-25 00:37:10 +0900

    年齢計算のメソッドを追加

---
+++ src/components/SelfIntroduction.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container class="self_introduction">
+    <v-card class="my_info">
+      <v-img src="/public/shota_fukushige_icon.jpg" class="my_info__icon"></v-img>
+      <div class="my_info__overview">
+        <v-card-title class="my_info__ja_name">福重 章太</v-card-title>
+        <v-card-subtitle class="my_info__en_name">Fukushige Shota</v-card-subtitle>
+        <p class="my_info__age">{{ageDisp}}</p>
+        <p class="my_info__work">フルスタックエンジニア見習い</p>
+      </div>
+      <v-card-text class="my_info__history">
+        2012年に京セラ株式会社に入社。製造オペレーターとしての作業を行いながら、合間を縫ってパソコンスキルを活かした業務改善に取り組む。作業時間改善の累計は2790時間/月以上に及ぶ。元々パソコンに触れるのが好きでゲームや動画編集、パソコン本体の作成などを学生の頃から行う。業務改善を目的としたExcelVBAを学んだのをキッカケにプログラミングにハマり、自分のスキルとして身につけて仕事にしたいと強く思うようになる。
+        2019年9月末にて同社を退職。2019年10月よりweb系エンジニア見習いとして転職し、勉強に勤しむ毎日。
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    ageCalc: function (year, month, day) {
+      const birthY = year.toString()
+      const birthM = month.toString()
+      const birthD = day.toString()
+      const today = new Date()
+      const birthFull = parseFloat(`${birthY}${birthM.padStart(2, '0')}${birthD.padStart(2, '0')}`)
+      const todayFull = parseFloat(`${today.getFullYear()}${(today.getMonth() + 1).toString().padStart(2, '0')}${today.getDay().toString().padStart(2, '0')}`)
+
+      return Math.floor((todayFull - birthFull) / 10000)
+    }
+  },
+  computed: {
+    ageDisp: function () {
+      const birthY = 1993
+      const birthM = 8
+      const birthD = 18
+      const age = this.ageCalc(birthY, birthM, birthD)
+
+      return `${age}歳`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
