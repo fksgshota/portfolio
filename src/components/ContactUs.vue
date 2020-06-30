13186d3398728473a7033ac44596b305f717214a
fksgshota <fksg.shotaness@gmail.com>
2020-07-01 01:17:47 +0900

    お問い合わせの雛形出来た

---
+++ src/components/ContactUs.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <h1>Contact Us</h1>
+    <v-form>
+      <v-text-field label="Name or Company" placeholder="namae"></v-text-field>
+      <v-text-field label="email" placeholder="example@"></v-text-field>
+      <v-text-field label="subject" placeholder="youkenn"></v-text-field>
+      <v-textarea label="message"></v-textarea>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
