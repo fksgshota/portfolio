549e5f51d03615f64771c26025b129b61d54c249
fksgshota <fksg.shotaness@gmail.com>
2020-07-03 02:51:26 +0900

    ボタンの位置を調整

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app-bar class="header blue-grey darken-2">
-    <HeaderButton v-for="link in links" :key="link.name" :name="link.name" :id="link.id"></HeaderButton>
+    <v-container class="d-flex justify-space-around">
+      <HeaderButton v-for="link in links" :key="link.name" :name="link.name" :id="link.id"></HeaderButton>
+    </v-container>
   </v-app-bar>
 </template>
 <script>
